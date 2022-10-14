5567ec968c33cb389bc7da77abf3f234b77b6366
andersen51205 <andersen51205@gmail.com>
2022-10-14 15:52:47 +0800

    功能微調

--- js/HpQuest.js
+++ js/HpQuest.js
@@ -2,38 +2,45 @@
 const questData = [{
         title: "綜合1",
         image: "./image/HpQuest/Misc1.png",
+        url: "./HpQuest-Misc1.html",
     },{
         title: "綜合2",
         image: "./image/HpQuest/Misc2.png",
+        url: "./HpQuest-Misc2.html",
     },{
         title: "水世界",
         image: "./image/HpQuest/Aquarium1.png",
+        url: "./HpQuest-Aquarium1.html",
     },{
         title: "玩具城",
         image: "./image/HpQuest/Ludibrium1.png",
+        url: "./HpQuest-Ludibrium1.html",
     },{
         title: "神木村1",
         image: "./image/HpQuest/Leafre1.png",
+        url: "./HpQuest-Leafre1.html",
     },{
         title: "神木村2",
         image: "./image/HpQuest/Leafre2.png",
+        url: "./HpQuest-Leafre2.html",
     },{
         title: "神木村3",
         image: "./image/HpQuest/Leafre3.png",
+        url: "./HpQuest-Leafre3.html",
     },{
         title: "新葉城",
         image: "./image/HpQuest/NLC1.png",
+        url: "./HpQuest-NLC1.html",
     },{
         title: "新加坡",
         image: "./image/HpQuest/CBD1.png",
+        url: "./HpQuest-CBD1.html",
     },{
         title: "世界旅行",
         image: "./image/HpQuest/WorldTravel1.png",
+        url: "./HpQuest-WorldTravel1.html",
 }];
-// 任務內容查看按鈕
-function showQuestDetail() {
-    document.querySelector('#Btn_quest_detail').click();
-}
+
 // Vue實例
 const questDetail = Vue.createApp({
     data() {
@@ -41,4 +48,9 @@ const questDetail = Vue.createApp({
             questData
         }
     }
-}).mount('#Div_quest_detail');
+}).mount('#Div_quest_content');
+
+// 任務內容查看按鈕
+function showQuestContent() {
+    document.querySelector('#Btn_quest_content').click();
+}
