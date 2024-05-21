b0e6022ebc1c023d01796ed93957fbc9764b6d4b
andersen51205 <andersen51205@gmail.com>
2024-05-22 00:39:34 +0800

    CWKPQ前置任務 蘇白尼的遺物1

---
+++ js/CwkpqPrequest.js
@@ -0,0 +1,20 @@
+// Vue實體
+// const questFlowVm = Vue.createApp({
+//     data() {
+//         return {
+//             questFlowDetails
+//         }
+//     }
+// }).mount('#Div_quest_flow');
+
+// 切換頁面
+function showPage(page) {
+    document.querySelector('#Div_quest_info').classList.add('d-none');
+    document.querySelector('#Div_subanis_legacy_quest_flow').classList.add('d-none');
+    if(page === "info") {
+        document.querySelector('#Div_quest_info').classList.remove('d-none');
+    }
+    else if(page === "subanis_legacy") {
+        document.querySelector('#Div_subanis_legacy_quest_flow').classList.remove('d-none');
+    }
+}
