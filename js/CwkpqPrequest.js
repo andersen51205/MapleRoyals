8332213a6689ab94592cc51acd7fcbe8ac4dc25f
andersen51205 <andersen51205@gmail.com>
2024-05-22 21:28:30 +0800

    CWKPQ前置任務 迷失了

--- js/CwkpqPrequest.js
+++ js/CwkpqPrequest.js
@@ -11,10 +11,14 @@
 function showPage(page) {
     document.querySelector('#Div_quest_info').classList.add('d-none');
     document.querySelector('#Div_subanis_legacy_quest_flow').classList.add('d-none');
+    document.querySelector('#Div_lost_quest_flow').classList.add('d-none');
     if(page === "info") {
         document.querySelector('#Div_quest_info').classList.remove('d-none');
     }
     else if(page === "subanis_legacy") {
         document.querySelector('#Div_subanis_legacy_quest_flow').classList.remove('d-none');
+    }
+    else if(page === "lost") {
+        document.querySelector('#Div_lost_quest_flow').classList.remove('d-none');
     }
 }
