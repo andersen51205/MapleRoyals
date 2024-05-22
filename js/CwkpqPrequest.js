0075373ce2001503f76974f6f799e7f14617db26
andersen51205 <andersen51205@gmail.com>
2024-05-22 21:28:36 +0800

    CWKPQ前置任務 切換頁面功能調整

--- js/CwkpqPrequest.js
+++ js/CwkpqPrequest.js
@@ -9,16 +9,8 @@
 
 // 切換頁面
 function showPage(page) {
-    document.querySelector('#Div_quest_info').classList.add('d-none');
-    document.querySelector('#Div_subanis_legacy_quest_flow').classList.add('d-none');
-    document.querySelector('#Div_lost_quest_flow').classList.add('d-none');
-    if(page === "info") {
-        document.querySelector('#Div_quest_info').classList.remove('d-none');
-    }
-    else if(page === "subanis_legacy") {
-        document.querySelector('#Div_subanis_legacy_quest_flow').classList.remove('d-none');
-    }
-    else if(page === "lost") {
-        document.querySelector('#Div_lost_quest_flow').classList.remove('d-none');
-    }
+    document.querySelectorAll('.page-content').forEach(function (el) {
+        el.classList.add('d-none');
+    })
+    document.querySelector(`.${page}`).classList.remove('d-none');
 }
