bfd0fa116184033c0813f6e8cdb1e41a8ba21bbc
andersen51205 <andersen51205@gmail.com>
2024-08-26 23:02:16 +0800

    新增 MapData 檔

---
+++ js/MapData.js
@@ -0,0 +1,37 @@
+/**
+ * Data Format
+ * <map_name>: {
+ *      img: <path/to/img.png>,
+ *      title: <map_title>,
+ * }
+ */
+const MAP_BASE_PATH = 'src/Map';
+const MAP_DATA = {
+    ElNath: {
+        img: MAP_BASE_PATH+'/ElnathMts/ElNath.png',
+        title: '冰原雪域：冰原雪域',
+    },
+    EosTower4155Floor: {
+        img: MAP_BASE_PATH+'/LudusLake/EosTower41-55f.png',
+        title: '愛奧斯塔：41~55樓',
+    },
+    Leafre: {
+        img: MAP_BASE_PATH+'/MinarForest/Leafre.png',
+        title: '神木村：神木村',
+    },
+    ValleyOfTheAntelope: {
+        img: MAP_BASE_PATH+'/MinarForest/ValleyOfTheAntelope.png',
+        title: '神木村：山羊峽谷',
+    },
+};
+const MINIMAP_BASE_PATH = 'src/Minimap'
+const MINIMAP_DATA = {
+    ChiefsResidence: {
+        img: MINIMAP_BASE_PATH+'/ElnathMts/ChiefsResidence.png',
+        title: '「冰源雪域：長老公館」入口處',
+    },
+    ForestOfThePriest: {
+        img: MINIMAP_BASE_PATH+'/MinarForest/ForestOfThePriest.png',
+        title: '「神木村：祭司之林」入口處',
+    },
+};
